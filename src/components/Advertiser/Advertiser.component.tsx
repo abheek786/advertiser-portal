1ea2478a2d63a6500cde728bd67035471c3ed394
abheek786 <rabheek@gmail.com>
2022-02-20 13:08:23 -0500

    Design update for Advertiser component

--- src/components/Advertiser/Advertiser.component.tsx
+++ src/components/Advertiser/Advertiser.component.tsx
@@ -3,14 +3,14 @@ import './Advertiser.component.scss';
 
 const Advertiser: FC = () => (
   <div className="Advertiser">
-    <h2>Create Advertiser</h2>
-    <div>
-      Name: <input />
-      APY: <input />
-      Minimum Balance: <input />
-      Logo: <input />
+    <h2 className="Advertiser-headline">Create Advertiser</h2>
+    <div className="Advertiser-formInput">
+      <div><input className="Advertiser-input" type="text" placeholder="Name"/></div>
+      <div><input className="Advertiser-input" type="text" placeholder="APY"/></div>
+      <div><input className="Advertiser-input" type="text" placeholder="Minimum Balance"/></div>
+      <div><input className="Advertiser-input" type="text" placeholder="Logo"/></div >
     </div>
-    <button type="button">Create</button>
+    <button type="button" className="Advertiser-create">Create</button>
   </div>
 );
 
