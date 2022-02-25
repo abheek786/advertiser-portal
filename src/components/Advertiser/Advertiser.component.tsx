d8fec3e0cbad579236daf45600e728ec6e50ff8b
abheek786 <rabheek@gmail.com>
2022-02-25 14:29:02 -0500

    Testing

--- src/components/Advertiser/Advertiser.component.tsx
+++ src/components/Advertiser/Advertiser.component.tsx
@@ -5,10 +5,10 @@ const Advertiser: FC = () => (
   <div className="Advertiser">
     <h2 className="Advertiser-headline">Create Advertiser</h2>
     <div className="Advertiser-formInput">
-      <div><input className="Advertiser-input" type="text" placeholder="Name"/></div>
-      <div><input className="Advertiser-input" type="text" placeholder="APYS"/></div>
-      <div><input className="Advertiser-input" type="text" placeholder="Minimum Balance"/></div>
-      <div><input className="Advertiser-input" type="text" placeholder="Logo"/></div >
+      <div><input className="Advertiser-input" type="text" placeholder="Name" /></div>
+      <div><input className="Advertiser-input" type="text" placeholder="APY" /></div>
+      <div><input className="Advertiser-input" type="text" placeholder="Minimum Balance" /></div>
+      <div><input className="Advertiser-input" type="text" placeholder="Logo" /></div >
     </div>
     <button type="button" className="Advertiser-create">Create</button>
   </div>
