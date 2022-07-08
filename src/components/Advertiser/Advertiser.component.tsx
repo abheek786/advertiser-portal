c4a3fc8f2aab94a422f3e9c27bf7a61e7b14a2a7
abheek786 <rabheek@gmail.com>
2022-07-08 11:28:48 -0400

    Circle CI Demo to Oscar and Sohila

--- src/components/Advertiser/Advertiser.component.tsx
+++ src/components/Advertiser/Advertiser.component.tsx
@@ -3,7 +3,7 @@ import './Advertiser.component.scss';
 
 const Advertiser: FC = () => (
   <div className="Advertiser">
-    <h2 className="Advertiser-headline">Create Advertiser</h2>
+    <h2 className="Advertiser-headline">Create Advertisers</h2>
     <div className="Advertiser-formInput">
       <div><input className="Advertiser-input" type="text" placeholder="Name" /></div>
       <div><input className="Advertiser-input" type="text" placeholder="APYS" /></div>
